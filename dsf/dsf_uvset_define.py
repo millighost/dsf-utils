c269567d3fb5b1de75ceccf79bd61bc51fc7ecb0
Ras Han <millighost@googlemail.com>
2012-03-04 20:51:54 +0100

    uv-loader (only workflow)

--- dsf/dsf_uvset_define.py
+++ dsf/dsf_uvset_define.py
@@ -1,9 +1,23 @@
+import itertools
 
-#format:
-# 'uvs' { 'count': int, 'values': [ (triples) ] }
-# 'polygon_vertex_indices' [ (triple: fidx, vidx, uvidx) ]
-# 'vertex_indices': [ int ]
-# 'id': string
+def create_uv_layer (msh, name):
+  """create a new uv layer and return its name.
+  """
+  uvlayer = msh.uv_textures.new (name = name)
+  return uvlayer
+
+def fill_uv_coords (uvlib, msh, uvl):
+  """uvlib is an object that returns uv-coordinates.
+     msh is a mesh data object.
+     uvl is a uv-layer data object.
+     uvl must have the same length as mesh.faces.
+  """
+  # should not be necessary, but who knows what bmesh is doing.
+  assert (len (msh.faces) == len (uvl))
+  for (mshface, uvface) in itertools.zip_longest (msh.faces, uvl):
+    uvcoords = uvlib.get_uvs (mshface.index, mshface.vertices)
+    assert (len (uvcoords) == 2 * len (mshface.vertices))
+    uvface.uv_raw = uvcoords
 
 class dsf_uvset_define (object):
   """class to define uvsets; mainly the define_uvset function is exported.
@@ -11,6 +25,8 @@ class dsf_uvset_define (object):
   @classmethod
   def define_uvset (self, obj, uvlib):
     """uvlib is the object returned by the loader.
-       (currently it simply contains the uv_set_library from the dsf).
+       uvlib must implement get_name() and get_uvs (face, verts).
     """
-    pass
+    msh = obj.data
+    uvl = msh.uv_textures.new (uvlib.get_name ())
+    fill_uv_coords (uvlib, msh, uvl.data)
--- dsf/dsf_uvset_load.py
+++ dsf/dsf_uvset_load.py
@@ -1,6 +1,32 @@
-import json, logging
+import json, logging, random
 
 log = logging.getLogger ('import_uvset')
+
+#format:
+# 'uvs' { 'count': int, 'values': [ (triples) ] }
+# 'polygon_vertex_indices' [ (triple: fidx, vidx, uvidx) ]
+# 'vertex_indices': [ int ]
+# 'id': string
+# assumption: vertex_indices contains indices into uvs-array for each vertex.
+# @todo: check, if this is true.
+
+class dsf_uvset (object):
+  """class to get uv-coordinates from the dsf-data.
+  """
+  def __init__ (self, uvlib):
+    """initialize with a given uv-library (a single item
+       from the uv-set-library in the dsf.
+    """
+    self.name = uvlib['id']
+  def get_name (self):
+    """returns the name of the uvset.
+    """
+    return self.name
+  def get_uvs (self, face, verts):
+    """return a list of 2*len(verts) numbers representing
+       the uv-coordinates of the given face.
+    """
+    return [random.random ()] * 2 * len (verts)
 
 class dsf_uvset_load (object):
   """class to load data for definition of uvsets.
@@ -17,4 +43,4 @@ class dsf_uvset_load (object):
     if len (uvlibs) == 0:
       raise TypeError ('file does contain at least one uv set.')
     log.info ("found %d uv sets in %s", len (uvlibs), filename)
-    return uvlibs[0]
+    return dsf_uvset (uvlibs[0])
