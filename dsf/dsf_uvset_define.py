29e180931665df570c79d611d223c25e28e4385d
Ras Han <millighost@googlemail.com>
2012-05-07 02:17:56 +0200

    uvimport seems to work again

--- dsf/dsf_uvset_define.py
+++ dsf/dsf_uvset_define.py
@@ -3,8 +3,9 @@ import itertools
 def create_uv_layer (msh, name):
   """create a new uv layer and return its name.
   """
-  uvlayer = msh.uv_textures.new (name = name)
-  return uvlayer
+  uvt = msh.uv_textures.new (name = name)
+  uvl = msh.uv_layers[-1]
+  return uvl
 
 def fill_uv_coords (uvlib, msh, uvl):
   """uvlib is an object that returns uv-coordinates.
@@ -12,12 +13,18 @@ def fill_uv_coords (uvlib, msh, uvl):
      uvl is a uv-layer data object.
      uvl must have the same length as mesh.faces.
   """
-  # should not be necessary, but who knows what bmesh is doing.
-  assert (len (msh.faces) == len (uvl))
-  for (mshface, uvface) in itertools.zip_longest (msh.faces, uvl):
-    uvcoords = uvlib.get_uvs (mshface.index, mshface.vertices)
-    assert (len (uvcoords) == 2 * len (mshface.vertices))
-    uvface.uv_raw = uvcoords
+  uv_data = uvl.data
+  uvoff = 0
+  for mshpoly in msh.polygons:
+    # mshpoly.vertices contains the list of vertices, like [0, 1, 2, 3, 4]
+    # get the vertices from the uvlib
+    uvcoords = uvlib.get_uvs (mshpoly.index, mshpoly.vertices)
+    for uv_rel_idx in range (len (mshpoly.vertices)):
+      uv_pair = uvcoords[2*uv_rel_idx:2*uv_rel_idx+2]
+      uv_abs_idx = uvoff + uv_rel_idx
+      uv_data[uv_abs_idx].uv = uv_pair
+    uvoff += len (mshpoly.vertices)
+  msh.update ()
 
 class dsf_uvset_define (object):
   """class to define uvsets; mainly the define_uvset function is exported.
@@ -28,5 +35,5 @@ class dsf_uvset_define (object):
        uvlib must implement get_name() and get_uvs (face, verts).
     """
     msh = obj.data
-    uvl = msh.uv_textures.new (uvlib.get_name ())
-    fill_uv_coords (uvlib, msh, uvl.data)
+    uvl = create_uv_layer (msh, uvlib.get_name ())
+    fill_uv_coords (uvlib, msh, uvl)
