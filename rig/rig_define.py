c55267421b0dc3e00e7b3b5b9a885c7e321124ab
Ras Han <millighost@googlemail.com>
2012-05-12 13:56:03 +0200

    up

--- rig/rig_define.py
+++ rig/rig_define.py
@@ -27,25 +27,46 @@ def transform_bone (orient, origin, bbone):
   # work; the bone will always rotate a bit. So use the translate()-method.
   bbone.translate (mathutils.Vector (origin))
 
+class bbone_map (dict):
+  """stores mapping from a blender bone to an armature bone.
+     attributes per mapping include:
+     bone - a link to the armature bone
+     roots - names of the blender bone that is to be used as a child for other
+       blender bones connecting to the armature bone.
+     leaf - name of the blender bone that is to be used as a parent for other
+       blender bones connecting to the armature bone.
+     axes - list of bone axis names that this bone represents.
+  """
+  def __init__ (self, *arg, **kwarg):
+    """initialize an empty bone map.
+    """
+    super (bbone_map, self).__init__ (*arg, **kwarg)
+  def get_leaf (self, id):
+    """get the blender bone that represents the tail of the bones
+       that were created for the bone with the id.
+    """
+    for b_info in self.values ():
+      if b_info.bone.get ('id') == id:
+        return b_info.leaf
+    return None
+
 class bone_info (object):
   """store data on a created bone or bones.
      this is for the case when a single input bone would result in multiple
      blender bones to be created.
   """
-  def __init__ (self, id = None):
-    """create a new bone info object. id is the name of a single bone
-       that leads to creation of this object.
+  def __init__ (self, bone = None, bname = None):
+    """create a new entry for the given blender bone name bname.
+       bone is the armature bone that lead to creation of this entry.
+       bname is the name of the blender bone (which is also used as a key).
     """
-    self.id = id
-    # bbones is the set of names of all blender bones that make
-    # this bone.
-    self.bbones = []
-    # end gives the name of a blender bone that is used as a parent
-    # whenever this bone is to have children.
-    self.end = None
-    # start is a set of names of blender bones that are to be used
-    # whenever this bone needs to be connected to a parent.
-    self.start = []
+    assert (bone)
+    assert (bname)
+    self.bone = bone
+    self.bname = bname
+    self.roots = []
+    self.leaf = None
+    self.axes = []
 
 def insert_bone (si_bone, armdat):
   """insert the bone object into the armature data armdat and returns
@@ -55,9 +76,9 @@ def insert_bone (si_bone, armdat):
      orientation, origin - basic position data of the bone used to create
        the transformation of the bone in armature-space.
   """
-  b_info = bone_info (id = si_bone.get ('id'))
-  bbone_name = "def-%s.xyz" % (si_bone.get ('id'))
-  b_bone = armdat.edit_bones.new (name = bbone_name)
+  bname = "def-%s.xyz" % (si_bone.get ('id'))
+  b_info = bone_info (bone = si_bone, bname = bname)
+  b_bone = armdat.edit_bones.new (name = bname)
   orient = si_bone.get ('orientation')
   origin = si_bone.get ('origin')
   # create an initial orientation by setting the tail of
@@ -66,17 +87,16 @@ def insert_bone (si_bone, armdat):
   b_bone.head = (0, 0, 0)
   b_bone.tail = (0, si_bone.get ('length'), 0)
   transform_bone (orient, origin, b_bone)
-  b_info.bbones.append (bbone_name)
-  b_info.start.append (bbone_name)
-  b_info.end = bbone_name
-  return b_info
+  b_info.roots.append (bname)
+  b_info.leaf = bname
+  return [b_info]
   
 def insert_bones (si_arm, armdat):
   """traverse the bones of the armature in preorder (parent before
      children) and insert them into the armature data.
      Returns a mapping of the names of the inserted bones to their definition.
   """
-  bone_mapping = dict ()
+  bone_mapping = bbone_map ()
   # the parent queue holds bone_info-objects whose children still 
   # need to get created.
   parent_queue = [None]
@@ -86,18 +106,25 @@ def insert_bones (si_arm, armdat):
     if parent is not None:
       # the parent is a b-info object and the b-infos end-attribute contains
       # the blender-bone to which children need to get linked to.
-      children = list (si_arm.get_children (parent.id))
-      parent_bbone = armdat.edit_bones[parent.end]
+      children = list (si_arm.get_children (parent.get ('id')))
+      parent_bname = bone_mapping.get_leaf (parent.get ('id'))
+      parent_bbone = armdat.edit_bones[parent_bname]
     else:
       children = list (si_arm.get_children (None))
       parent_bbone = None
     for child in children:
-      b_info = insert_bone (child, armdat)
-      for bbone_start_name in b_info.start:
-        bbone_start = armdat.edit_bones[bbone_start_name]
-        bbone_start.parent = parent_bbone
-      bone_mapping[b_info.id] = b_info
-      parent_queue.append (b_info)
+      # create the bbones representing the child. This returns a list
+      # of b-info records.
+      b_infos = insert_bone (child, armdat)
+      # link all blender bones to the parent
+      for b_info in b_infos:
+        # a blender bone might have multiple roots. assign the parent
+        # to each of them.
+        for bname in b_info.roots:
+          bbone_start = armdat.edit_bones[bname]
+          bbone_start.parent = parent_bbone
+        bone_mapping[b_info.bname] = b_info
+      parent_queue.append (child)
   return bone_mapping
 
 # poser/daz bones are always based on euler rotations and might contain
@@ -109,12 +136,11 @@ def configure_bones (armdat, bone_mapping, armobj):
   """perform final fixups on the created bones.
   """
   pose_bones = armobj.pose.bones
-  for (bone_name, b_info) in bone_mapping.items ():
-    si_bone = armdat.get_bone (b_info.id)
+  for (bname, b_info) in bone_mapping.items ():
+    bbone = pose_bones[bname]
+    si_bone = b_info.bone
     order = si_bone.get ('rotation_order')
-    for bbone_name in b_info.bbones:
-      bbone = pose_bones[bbone_name]
-      bbone.rotation_mode = order
+    bbone.rotation_mode = order
 
 def define_armature (si_arm, ctx):
   """create a blender-armature object from the given armature-data.
