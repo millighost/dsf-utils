27bf369241c8ed2642064aeecd1e88db3b5e839e
Ras Han <millighost@googlemail.com>
2012-07-10 00:39:36 +0200

    only dsf

--- serial/__init__.py
+++
@@ -1,3 +0,0 @@
-import serial.c_image
-import serial.c_material
-import serial.c_node
--- testc.py
+++
@@ -1,9 +0,0 @@
-import mathutils
-import bpy
-import math
-
-import t
-
-import cr2.rig_define as rdef
-arm_a = t.arm_a
-arm_ss = t.arm_ss
